246e891a92d520d10dd23d33261d6f8dab3c2dc9
Shan Baluyos <shan.baluyos21@gmail.com>
2026-03-16 18:32:31 +0100

    added banner html, css, and javascript

--- script.js
+++ script.js
@@ -50,3 +50,25 @@ navLinks.forEach(link => {
         menu.classList.remove('show');
     });
 });
+
+// Hero banner slider
+
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+function showSlide() {
+
+    slides.forEach(slide => {
+        slide.classList.remove("active");
+    });
+
+    currentSlide++;
+
+    if (currentSlide >= slides.length) {
+        currentSlide = 0;
+    }
+
+    slides[currentSlide].classList.add("active");
+}
+
+setInterval(showSlide, 5000);
